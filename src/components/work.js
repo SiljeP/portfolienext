7073594d78f23208f5f0e52408a875b953faea78
SiljeP <u2440913@rts-365.dk>
2024-02-26 09:23:56 +0100

    et eller andet

--- src/components/work.js
+++ src/components/work.js
@@ -3,8 +3,9 @@
 export default function Work(){
     return(
         <>
-       <h1 className="font-title text-white ">Work</h1>
-       <p className="font-body">Most of my school projects have been before learning React and therefore this page, my portfolio, is properly the most up to date project I have. In this portfolio I make use of: React, next.js, tailwind, framer motion for animations, typed.js library and many more.  </p>
+       <h1 className="font-title text-white text-6xl text-center">Work</h1>
+       <p className="font-body text-xl text-center ">Most of my school projects have been before learning React and therefore this page, my portfolio, is properly the most up to date project I have. In this portfolio I make use of: React, next.js, tailwind, framer motion for animations, typed.js library and more.</p>
+       <p>Other projects, from before React, includes working with webapi&apos;s on a pokedex, check out here</p>
         </>
     )
 }
